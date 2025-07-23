89193e307e9a8261de49152e51aa7d6bb7ed4f9c
曦月 <xiyuesaves@gmail.com>
2025-07-23 19:39:45 +0800

    Revert "chore: 重写更新日志生成逻辑"
    This reverts commit a1e55a7099040a675b637005850a8acea041931f.

--- esbuild.config.mjs
+++ esbuild.config.mjs
@@ -1,5 +1,6 @@
 import { buildSync } from "esbuild";
 import * as sass from "sass";
+import { getAllRelease } from "./createChangeLog.mjs";
 import fs from "fs";
 let thisTime = new Date().getTime();
 const lite_tools = JSON.parse(fs.readFileSync("./package.json", "utf-8"));
@@ -50,5 +51,13 @@ if (isPush) {
 manifest_json.version = `${lite_tools.version}${isDev ? "-dev" : ""}`;
 manifest_json.repository.release.tag = `v${lite_tools.version}`;
 fs.writeFileSync("./manifest.json", JSON.stringify(manifest_json, null, 2));
+
+// 生成更新日志
+if (!isDev && !isPush) {
+  console.log("生成更新日志...");
+  await getAllRelease();
+} else {
+  console.log("跳过生成更新日志");
+}
 
 console.log("构建完成");
