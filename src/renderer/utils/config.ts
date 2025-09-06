6d0bc822550bca18567e32336d6b6d4c56edc644
曦月 <xiyuesaves@gmail.com>
2025-09-06 17:34:52 +0800

    feat: 优化renderer的配置模块

--- src/renderer/utils/config.ts
+++ src/renderer/utils/config.ts
@@ -6,8 +6,26 @@ type ConfigListener = (config: Config) => void;
 
 class ConfigStore {
   private listeners: Set<ConfigListener> = new Set();
+  private readyPromise: Promise<void>;
 
-  config: Config;
+  config!: Config;
+
+  constructor() {
+    // 异步获取初始配置
+    this.readyPromise = lite_tools.getConfig().then((config: Config) => {
+      this.config = config;
+
+      // 订阅主进程广播的更新
+      lite_tools.onConfigChange((update: Config) => {
+        Object.assign(this.config, update);
+        this.notify();
+      });
+    });
+  }
+
+  async ready() {
+    return this.readyPromise;
+  }
 
   setConfig(update: Config) {
     Object.assign(this.config, update);
@@ -28,14 +46,6 @@ class ConfigStore {
 
   private notify() {
     this.listeners.forEach((listener) => listener(this.config));
-  }
-
-  constructor() {
-    this.config = lite_tools.getConfig();
-    lite_tools.onConfigChange((config: Config) => {
-      Object.assign(this.config, config);
-      this.notify();
-    });
   }
 }
 
