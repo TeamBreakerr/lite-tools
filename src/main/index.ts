420cb1c6902908ba1da26a9eb6e3534d53ed23a6
曦月 <xiyuesaves@gmail.com>
2025-09-06 14:56:36 +0800

    feat: main增加基础功能

--- src/main/index.ts
+++ src/main/index.ts
@@ -1,10 +1,30 @@
-import { BrowserWindow } from "electron";
-function onBrowserWindowCreated(browserWindow: BrowserWindow): void {
-  
+import { setupConfig } from "./modules/config";
+import createLogger from "./utils/createLogger";
+import type { BrowserWindow } from "electron";
+
+if (!("IpcInterceptor" in globalThis)) {
+  throw new Error("请安装前置插件 ipc_interceptor");
 }
+
+const log = createLogger("lt_main");
+
+log("插件启动");
+
+const unSubscribe = IpcInterceptor.onIpcSendEvents("nodeIKernelSessionListener/onSessionInitComplete", (...args) => {
+  log("获取到UID", args);
+  setupConfig(args[2].payload.uid);
+  unSubscribe();
+});
+
+function onBrowserWindowCreated(browserWindow: BrowserWindow) {}
 
 if ("qwqnt" in globalThis) {
   qwqnt.main.hooks.whenBrowserWindowCreated.peek(onBrowserWindowCreated);
 }
+
+// 错误处理
+process.on("uncaughtException", (e) => {
+  log("主进程出错", e, e?.stack);
+});
 
 export default { onBrowserWindowCreated };
