7683c83f8523b3354b240e12cabf0139323fd240
曦月 <xiyuesaves@gmail.com>
2025-09-06 14:50:47 +0800

    chore: 调整类型声明

---
+++ src/common/types.ts
@@ -0,0 +1,3 @@
+import configJson from "./config.template.json";
+
+export type Config = typeof configJson;
--- src/global.d.ts
+++ src/global.d.ts
@@ -17,6 +17,12 @@ interface Window {
   lt_errors: () => void;
 }
 
+// LiteLoader 兼容
+declare const LiteLoader: any;
+
+// ipc-logger 兼容
+declare const Logs: any;
+
 // RendererEvents
 declare namespace RendererEvents {
   const onSettingsWindowCreated: (callback: () => void) => void;
@@ -50,3 +56,23 @@ declare namespace PluginSettings {
   const preload: ICommon;
   const renderer: IRenderer;
 }
+
+
+// ipc-interceptor.d.ts
+
+type Unsubscribe = () => void;
+type EventName = string | string[];
+type IpcCallback = (...args: any[]) => void;
+
+interface IpcInterceptorType {
+  onIpcReceive(callback: IpcCallback): Unsubscribe;
+  onIpcSend(callback: IpcCallback): Unsubscribe;
+  offIpcReceive(callback: IpcCallback): void;
+  offIpcSend(callback: IpcCallback): void;
+  onIpcReceiveEvents(eventName: EventName, callback: IpcCallback): Unsubscribe;
+  onIpcSendEvents(eventName: EventName, callback: IpcCallback): Unsubscribe;
+  offIpcReceiveEvents(eventName: EventName, callback: IpcCallback): void;
+  offIpcSendEvents(eventName: EventName, callback: IpcCallback): void;
+}
+
+declare const IpcInterceptor: IpcInterceptorType;
