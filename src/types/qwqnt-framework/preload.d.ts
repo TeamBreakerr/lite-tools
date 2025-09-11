9f0fdcbd356dc8dd9106caaad847b84f097bae2a
xiyuesaves <xiyuesaves@gmail.com>
2025-09-11 15:24:32 +0800

    chore: 调整 qwqnt types 导入方式

--- src/types/qwqnt-framework/preload.d.ts
+++
@@ -1,9 +0,0 @@
-import { C as ContextGlobal } from './types-context-CFIIPswK.js';
-
-type ContextPreload = ContextGlobal;
-declare global {
-    const qwqnt: Readonly<ContextPreload>;
-    function evalModule(script: string): void;
-}
-
-export type { ContextPreload };
--- src/types/qwqnt-framework/renderer.d.ts
+++
@@ -1,8 +0,0 @@
-import { C as ContextGlobal } from './types-context-CFIIPswK.js';
-
-type ContextRenderer = ContextGlobal;
-declare global {
-    const qwqnt: Readonly<ContextRenderer>;
-}
-
-export type { ContextRenderer };
