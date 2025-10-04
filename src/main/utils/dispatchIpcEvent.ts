e12f8fbdcdb473a82ceb1fb73cb9f7ca3ac5ef53
xiyuesaves <xiyuesaves@gmail.com>
2025-10-05 00:20:31 +0800

    feat: 新增派遣事件工具函数

---
+++ src/main/utils/dispatchIpcEvent.ts
@@ -0,0 +1,95 @@
+import { webContents } from "electron";
+import { randomUUID } from "crypto";
+import { createLogger } from "@/main/utils/createLogger";
+
+const log = createLogger("dispatchIpcEvent");
+
+function dispatchIpcEvent(
+  webContentId: number,
+  event: any,
+  payload: any,
+  awaitCallback?: boolean | string | string[]
+): Promise<any> {
+  const webContent = webContents.fromId(webContentId);
+  if (!webContent) {
+    return Promise.resolve(null);
+  }
+  const callbackId = randomUUID();
+  const ipcFromMain = `RM_IPCFROM_MAIN${webContentId}`;
+  const ipcFromRenderer = `RM_IPCFROM_RENDERER${webContentId}`;
+  let resolve;
+  if (awaitCallback) {
+    resolve = new Promise((res) => {
+      function onEvent(channel: string, ...args: any[]): InterceptResult {
+        if (channel === ipcFromMain) {
+          if (typeof awaitCallback === "boolean") {
+            if (args[0]?.callbackId === callbackId) {
+              unsubscribe();
+              res(args[1]);
+              return {
+                action: "block",
+              };
+            }
+          } else if (Array.isArray(awaitCallback)) {
+            if (awaitCallback.includes(args?.[1]?.cmdName)) {
+              unsubscribe();
+              res(args[1]);
+              return {
+                action: "block",
+              };
+            }
+          } else {
+            if (args?.[1]?.cmdName === awaitCallback) {
+              unsubscribe();
+              res(args[1]);
+              return {
+                action: "block",
+              };
+            }
+          }
+        }
+        return {
+          action: "pass",
+        };
+      }
+      const unsubscribe = IpcInterceptor.interceptIpcSend(onEvent);
+    });
+  } else {
+    resolve = Promise.resolve(null);
+  }
+
+  const emitData = [
+    ipcFromRenderer,
+    {
+      peerId: webContentId,
+      callbackId,
+      ...event,
+    },
+    payload,
+  ];
+
+  webContent.emit(
+    "-ipc-message",
+    {
+      frameId: 1,
+      frameTreeNodeId: 2,
+      sender: webContent,
+      processId: 5,
+      senderFrame: {},
+    },
+    false,
+    ipcFromRenderer,
+    [
+      {
+        peerId: webContentId,
+        callbackId,
+        ...event,
+      },
+      payload,
+    ]
+  );
+  log("emitData", emitData);
+  return resolve;
+}
+
+export { dispatchIpcEvent };
