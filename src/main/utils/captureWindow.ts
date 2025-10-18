57faa77213017e27ea3c73ee31d3c5262742e8a0
xiyuesaves <xiyuesaves@gmail.com>
2025-10-19 02:21:25 +0800

    feat: 优化工具函数逻辑

--- src/main/utils/captureWindow.ts
+++ src/main/utils/captureWindow.ts
@@ -7,10 +7,20 @@ function captureWindow(window: BrowserWindow) {
   window.webContents.on("did-stop-loading", () => {
     if (window.webContents.getURL().indexOf("#/main/message") !== -1) {
       mainWindow = window;
+      handleWindowClose(window);
     }
     if (window.webContents.getURL().indexOf("#/setting/settings/common") !== -1) {
       settingWindow = window;
+      handleWindowClose(window);
     }
   });
 }
+
+function handleWindowClose(window: BrowserWindow) {
+  window.once("close", () => {
+    if (window === mainWindow) mainWindow = null;
+    else if (window === settingWindow) settingWindow = null;
+  });
+}
+
 export { captureWindow, mainWindow, settingWindow };
