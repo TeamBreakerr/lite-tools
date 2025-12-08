edf63c21fcc8c7ef24f04c4ef28e2689b9f6d064
xiyuesaves <xiyuesaves@gmail.com>
2025-12-08 15:02:31 +0800

    feat: ipc广播模块增加url白名单

--- src/main/utils/globalBroadcast.ts
+++ src/main/utils/globalBroadcast.ts
@@ -1,7 +1,10 @@
 import { BrowserWindow } from "electron";
 import { createLogger } from "@/main/utils/createLogger";
 
-const log = createLogger("globalBroadcast");
+const log = createLogger("globalBroadcast", true);
+
+// 白名单
+const whiteList = new Set(["main", "chat", "setting", "record", "forward"]);
 
 let pendingBroadcasts: { channel: string; data: any[] }[] = [];
 let broadcastScheduled = false;
@@ -19,7 +22,13 @@ function globalBroadcast(channel: string, ...data: any[]) {
         if (!window.isDestroyed()) {
           try {
             for (const b of broadcasts) {
-              window.webContents.send(b.channel, ...b.data);
+              const url = window.webContents.getURL();
+              const hash = url.split("#")[1] || "";
+              const firstSegment = hash.split("/")[1];
+              if (whiteList.has(firstSegment)) {
+                log("send", firstSegment, b.channel);
+                window.webContents.send(b.channel, ...b.data);
+              }
             }
           } catch (err) {
             log("广播失败:", err);
