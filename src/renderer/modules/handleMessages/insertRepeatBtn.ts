3e1fe811c321287015e2625591aba34443d81988
xiyuesaves <xiyuesaves@gmail.com>
2026-01-06 15:41:45 +0800

    refactor: 拆分handleMessages模块

---
+++ src/renderer/modules/handleMessages/insertRepeatBtn.ts
@@ -0,0 +1,61 @@
+import { forwardMessage } from "@/renderer/utils/nativeCall";
+import { configStore } from "@/renderer/modules/configStore";
+
+import { ElementType } from "./ElementTypeEnum";
+import { createSlot } from "./messageSlot";
+
+import type { MessageElement, SlotElement } from "./type";
+
+const repeatElementType = [ElementType.textElement, ElementType.picElement];
+
+function insertRepeatBtn(aioStore: any, slot: SlotElement, msgRecord: any, messageEl: MessageElement) {
+  if (messageEl.querySelector(".lt-repeat")) return;
+
+  if (
+    msgRecord.elements.some((element: any) => {
+      return repeatElementType.includes(element.elementType);
+    })
+  ) {
+    let doubleClick = false;
+    const btn = document.createElement("span");
+    const msgId = msgRecord.msgId;
+    btn.classList.add("lt-repeat");
+    btn.textContent = "+1";
+    btn.addEventListener("click", () => {
+      const peer = {
+        chatType: aioStore.value.chatType,
+        guildId: "",
+        peerUid: aioStore.value.header.uid,
+      };
+      if (configStore.value.message.repeatMessage.doubleClick) {
+        setTimeout(() => {
+          doubleClick = false;
+        }, 500);
+        if (doubleClick) {
+          forwardMessage(peer, peer, [msgId]);
+          doubleClick = false;
+        }
+        doubleClick = true;
+      } else {
+        forwardMessage(peer, peer, [msgId]);
+      }
+    });
+    if (slot.classList.contains("outside")) {
+      const float = slot.children[1] as HTMLElement;
+      float?.insertAdjacentElement("beforeend", btn);
+    } else {
+      const privateSlot = createSlot();
+      privateSlot.classList.add("outside");
+      if (!messageEl.querySelector(".content-status.no-copy")) {
+        const div = document.createElement("div");
+        div.classList.add("content-status", "no-copy", "lt-add");
+        messageEl.querySelector(".message-content__wrapper")?.insertAdjacentElement("afterend", div);
+      }
+      privateSlot.querySelector(".lt-float")?.appendChild(btn);
+      // log("插入外部插槽");
+      messageEl.querySelector(".content-status.no-copy")?.appendChild(privateSlot);
+    }
+  }
+}
+
+export { insertRepeatBtn };
---
+++ src/renderer/modules/handleMessages/messageRecall.ts
@@ -0,0 +1,36 @@
+import { formatChineseDate } from "@/renderer/modules/handleMessages/formatChineseDate";
+
+import type { LiteTools } from "@/preload";
+import type { SlotElement } from "./type";
+
+declare const lite_tools: LiteTools;
+
+function insertRecallTag(slot: SlotElement, msgRecord: any) {
+  if (msgRecord.lt_recall) {
+    if (slot.querySelector(".lt-recall")) return;
+    const span = document.createElement("span");
+    span.classList.add("lt-recall");
+    span.textContent = "已撤回";
+    span.title = `${formatChineseDate(new Date(msgRecord.msgTime * 1000))} 被 ${
+      msgRecord.lt_recall.operatorRemark || msgRecord.lt_recall.operatorMemRemark || msgRecord.lt_recall.operatorNick
+    } 撤回`;
+    const clone = span.cloneNode(true) as HTMLElement;
+    const spacer = slot.children[0] as HTMLElement;
+    const float = slot.children[1] as HTMLElement;
+    spacer?.insertAdjacentElement("afterbegin", span);
+    float?.insertAdjacentElement("afterbegin", clone);
+  }
+}
+
+function initRecallMessageListener(processMessages: any) {
+  lite_tools.onRecallMessagesFound((recallDatas) => {
+    for (const recallData of recallDatas) {
+      const recallMsg = document.getElementById(recallData.id.toString())?.firstElementChild;
+      if (recallMsg && recallMsg?.__VUE__?.[0]) {
+        processMessages(recallMsg.__VUE__[0]);
+      }
+    }
+  });
+}
+
+export { initRecallMessageListener, insertRecallTag };
