4fe924c8bb41402515417cd4d11d329d2b581d89
xiyuesaves <xiyuesaves@gmail.com>
2026-01-22 15:45:07 +0800

    chore: 添加注释

--- src/main/modules/ipcMain.ts
+++ src/main/modules/ipcMain.ts
@@ -6,11 +6,13 @@ function setupIpcMain() {
   ipcMain.on("lite_tools.getWebContentId", (event) => {
     event.returnValue = event.sender.id;
   });
+  // 使用主进程广播
   ipcMain.on("lite_tools.broadcast", (event, channelName, payload) => {
     if (mainWindow && mainWindow?.webContents?.send) {
       mainWindow.webContents.send("lite_tools.broadcast", channelName, payload);
     }
   });
+  // 通用文件选择弹窗
   ipcMain.handle("lite_tools.showOpenDialog", (event, options: Electron.OpenDialogOptions) => {
     const win = BrowserWindow.fromWebContents(event.sender);
     return dialog.showOpenDialog(win!, options);
