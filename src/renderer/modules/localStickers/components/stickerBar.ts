d543ed7018b867151db7273f8ebf6fc72a58768e
xiyuesaves <xiyuesaves@gmail.com>
2026-03-04 17:21:58 +0800

    feat: 新增图片加载失败时的占位图标

--- src/renderer/modules/localStickers/components/stickerBar.ts
+++ src/renderer/modules/localStickers/components/stickerBar.ts
@@ -30,6 +30,11 @@ export class StickerBarItem extends LitElement {
         transition: opacity 150ms;
         opacity: 0;
       }
+      svg {
+        width: 100%;
+        height: 100%;
+        color: #cdcdcd;
+      }
       &.active {
         background-color: var(--hover-background);
       }
@@ -42,6 +47,17 @@ export class StickerBarItem extends LitElement {
   @property({ type: String })
   public activeDirPath = "";
 
+  static ERR_ICON = html`<svg viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg">
+    <path
+      d="M854.5 82h-685c-58.2 0-105.4 49.2-105.4 109.8v640.4c0 60.6 47.2 109.8 105.4 109.8h685.1c58.2 0 105.4-49.2 105.4-109.8V191.8C959.9 131.2 912.7 82 854.5 82zM223.6 223.3l11.3-11.7c8.1-8.4 54.7 22.5 101.4 64.9 46.9-42.8 93.4-73.4 101.5-64.9l11.3 11.7c9.6 10-20.2 57.3-61.3 105.3 41.9 47.3 71.3 94.3 60.6 105.5l-11.3 11.7c-10.6 11-55.3-19.8-100.8-63.1-45.4 43-90.3 74-100.8 63.1L224.3 434c-10.5-11 19.3-58.3 60.7-105.5-40.9-47.8-70.9-95.3-61.4-105.2z m630.9 590.6c-12.9 41.9 0.6 36.6-333.7 36.6-338.1 0-342.2-0.8-351.3-47.8-14.2-73.5 64.8-246.2 122.9-208.3 24.9 16.2 81.4 72.5 105.4 73.2 126.9 3.9 212.1-237.9 298.6-237.9 82.3 0 190.8 278.1 158.1 384.2z"
+      fill="currentColor"
+      p-id="2104"
+    ></path>
+  </svg>`;
+
+  @state()
+  _isError = false;
+
   private _loadImage(e: Event) {
     const img = e.target as HTMLImageElement;
     img.style.opacity = "1";
@@ -49,7 +65,14 @@ export class StickerBarItem extends LitElement {
 
   render() {
     return html`<div class="lt-sticker-bar-item ${this.activeDirPath === this.stickerPack.dirPath ? "active" : ""}">
-      <img src="${this.stickerPack.icon!}" @load="${this._loadImage}" />
+      ${!this._isError
+        ? html`<img
+            loading="lazy"
+            src="appimg://${this.stickerPack.icon!}"
+            @error="${() => (this._isError = true)}"
+            @load="${this._loadImage}"
+          />`
+        : StickerBarItem.ERR_ICON}
     </div>`;
   }
 }
--- src/renderer/modules/localStickers/components/stickerPack.ts
+++ src/renderer/modules/localStickers/components/stickerPack.ts
@@ -31,12 +31,28 @@ export class StickerItem extends LitElement {
         opacity: 0;
         transition: opacity 300ms;
       }
+      svg {
+        width: 100%;
+        height: 100%;
+        color: #cdcdcd;
+      }
       &:hover {
         background-color: var(--hover-background);
         box-shadow: 0 0 0 3px var(--hover-background);
       }
     }
   `;
+
+  static ERR_ICON = html`<svg viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg">
+    <path
+      d="M854.5 82h-685c-58.2 0-105.4 49.2-105.4 109.8v640.4c0 60.6 47.2 109.8 105.4 109.8h685.1c58.2 0 105.4-49.2 105.4-109.8V191.8C959.9 131.2 912.7 82 854.5 82zM223.6 223.3l11.3-11.7c8.1-8.4 54.7 22.5 101.4 64.9 46.9-42.8 93.4-73.4 101.5-64.9l11.3 11.7c9.6 10-20.2 57.3-61.3 105.3 41.9 47.3 71.3 94.3 60.6 105.5l-11.3 11.7c-10.6 11-55.3-19.8-100.8-63.1-45.4 43-90.3 74-100.8 63.1L224.3 434c-10.5-11 19.3-58.3 60.7-105.5-40.9-47.8-70.9-95.3-61.4-105.2z m630.9 590.6c-12.9 41.9 0.6 36.6-333.7 36.6-338.1 0-342.2-0.8-351.3-47.8-14.2-73.5 64.8-246.2 122.9-208.3 24.9 16.2 81.4 72.5 105.4 73.2 126.9 3.9 212.1-237.9 298.6-237.9 82.3 0 190.8 278.1 158.1 384.2z"
+      fill="currentColor"
+      p-id="2104"
+    ></path>
+  </svg>`;
+
+  @state()
+  _isError = false;
 
   @state()
   _opacity = 0;
@@ -47,12 +63,15 @@ export class StickerItem extends LitElement {
   render() {
     return html`
       <div class="lt-sticker-item">
-        <img
-          loading="lazy"
-          style="opacity: ${this._opacity}"
-          @load="${() => (this._opacity = 1)}"
-          src="${this.sticker.path!}"
-        />
+        ${!this._isError
+          ? html`<img
+              loading="lazy"
+              style="opacity: ${this._opacity}"
+              @load="${() => (this._opacity = 1)}"
+              @error="${() => (this._isError = true)}"
+              src="appimg://${this.sticker.path!}"
+            />`
+          : StickerItem.ERR_ICON}
       </div>
     `;
   }
