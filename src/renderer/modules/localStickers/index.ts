452981768ec5be220d92498952c187c6d6ad5237
xiyuesaves <xiyuesaves@gmail.com>
2026-03-05 16:39:07 +0800

    feat: 添加setup函数

--- src/renderer/modules/localStickers/index.ts
+++ src/renderer/modules/localStickers/index.ts
@@ -6,7 +6,9 @@ import { StickerPack, StickerItem } from "./components/stickerPack";
 import { StickerIcon } from "./components/stickerIcon";
 import { StickerFullViewer } from "./components/stickerFullViewer";
 
-export { StickerPanel, StickerMsg, StickerList, StickerBar, StickerPack, StickerItem, StickerIcon, StickerFullViewer };
+import { configStore } from "@/renderer/modules/configStore";
+import { waitForElement } from "@/renderer/utils/domWaitFor";
+import { observeMutations } from "@/renderer/utils/observeMutations";
 
 declare global {
   interface HTMLElementTagNameMap {
@@ -20,3 +22,45 @@ declare global {
     "lt-sticker-full-viewer": StickerFullViewer;
   }
 }
+
+async function setupLocalStickers() {
+  await configStore.ready;
+  const injectPosition = ".chat-func-bar .func-bar-native.func-bar-shortcuts:last-child";
+  const stickerIcon = document.createElement("lt-sticker-icon");
+  let offObserver: ReturnType<typeof observeMutations> | null = null;
+
+  const updateIconState = async () => {
+    const isEnabled = configStore.value.localStickers.enabled;
+    const existingIcon = document.querySelector("lt-sticker-icon");
+
+    if (isEnabled) {
+      if (!existingIcon) {
+        const target = await waitForElement(injectPosition);
+        if (target) {
+          target.insertAdjacentElement("afterbegin", stickerIcon);
+          offObserver?.();
+          offObserver = observeMutations(target, updateIconState, {
+            childList: true,
+          });
+        }
+      }
+    } else {
+      existingIcon?.remove();
+      offObserver?.();
+    }
+  };
+  updateIconState();
+  configStore.onChange(updateIconState);
+}
+
+export {
+  StickerPanel,
+  StickerMsg,
+  StickerList,
+  StickerBar,
+  StickerPack,
+  StickerItem,
+  StickerIcon,
+  StickerFullViewer,
+  setupLocalStickers,
+};
