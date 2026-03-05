18a31e33fdbaed81dc91dcab2cfb1f0d1e11fce1
xiyuesaves <xiyuesaves@gmail.com>
2026-03-05 19:50:10 +0800

    feat: 添加未完成功能提示和事件监听逻辑

--- src/renderer/modules/localStickers/index.ts
+++ src/renderer/modules/localStickers/index.ts
@@ -9,6 +9,9 @@ import { StickerFullViewer } from "./components/stickerFullViewer";
 import { configStore } from "@/renderer/modules/configStore";
 import { waitForElement } from "@/renderer/utils/domWaitFor";
 import { observeMutations } from "@/renderer/utils/observeMutations";
+import { toastManager } from "@/renderer/modules/toastManager";
+import { sendMessage } from "@/renderer/utils/nativeCall";
+import { aioStore } from "@/renderer/modules/aioStore";
 
 declare global {
   interface HTMLElementTagNameMap {
@@ -26,7 +29,15 @@ declare global {
 async function setupLocalStickers() {
   await configStore.ready;
   const injectPosition = ".chat-func-bar .func-bar-native.func-bar-shortcuts:last-child";
-  const stickerIcon = document.createElement("lt-sticker-icon");
+  const stickerIcon = document.createElement("lt-sticker-icon") as StickerIcon;
+
+  const editor = (await waitForElement(".ck.ck-content.ck-editor__editable")) as any;
+
+  const ckeditorInstance = editor.ckeditorInstance;
+  const ckeditEditorModel = ckeditorInstance.model;
+
+  // ckeditEditorModel.document.on("change:data", debounceQuickInsertion);
+
   let offObserver: ReturnType<typeof observeMutations> | null = null;
 
   const updateIconState = async () => {
@@ -51,6 +62,27 @@ async function setupLocalStickers() {
   };
   updateIconState();
   configStore.onChange(updateIconState);
+
+  stickerIcon.addEventListener("lt-select-sticker", (e) => {
+    ckeditEditorModel.change((writer: any) => {
+      const picSubType = configStore.value.localStickers.sendAsPic ? 0 : 1;
+      const selection = ckeditEditorModel.document.selection;
+      const position = selection.getFirstPosition();
+      const writerEl = writer.createElement("msg-img", {
+        data: JSON.stringify({ type: "pic", src: e.detail.path, picSubType }),
+      });
+      writer.insert(writerEl, position);
+      writer.setSelection(writer.createPositionAt(writerEl, "after"));
+    });
+  });
+
+  stickerIcon.addEventListener("lt-send-sticker", (e) => {
+    const picSubType = configStore.value.localStickers.sendAsPic ? 0 : 1;
+    // 暂未实现
+    toastManager.show("暂未实现", "default", 1500);
+    // sendMessage(aioStore.getPeer(), [{ type: "image", path: e.detail.path, picSubType }]);
+    console.log("send", e);
+  });
 }
 
 export {
