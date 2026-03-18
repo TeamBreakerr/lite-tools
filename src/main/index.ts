2816f9c40abb14f02438769b3ad95859b9983905
xiyuesaves <xiyuesaves@gmail.com>
2026-03-18 16:17:22 +0800

    chore: 重命名模块名称

--- src/main/index.ts
+++ src/main/index.ts
@@ -1,7 +1,7 @@
 import { configManager } from "@/main/modules/configManager";
 import { createLogger } from "@/main/utils/createLogger";
 import { setupHandleMessages } from "@/main/modules/handleMessages";
-import { setupIpcMain } from "@/main/modules/ipcMain";
+import { setupIpcMain } from "@/main/modules/ipcRegistrar";
 import { setupSideBar } from "@/main/modules/sideBar";
 import { captureWindow } from "@/main/utils/captureWindow";
 import { setupIpcInterceptor } from "@/main/modules/ipcInterceptor";
