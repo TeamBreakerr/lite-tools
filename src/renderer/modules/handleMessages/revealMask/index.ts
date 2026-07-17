ceca36020e9e0924c082cba1967406dbd68ba785
xiyuesaves <xiyuesaves@gmail.com>
2026-07-17 16:51:00 +0800

    feat: 新增引用图片添加遮罩处理

--- src/renderer/modules/handleMessages/revealMask/index.ts
+++ src/renderer/modules/handleMessages/revealMask/index.ts
@@ -1,11 +1,15 @@
 import "./components/revealMask";
 import { importBlobAsset } from "@/renderer/utils/importAsset";
-import { resource } from "./resource";
 import { configStore } from "@/renderer/modules/configStore";
+import { createLogger } from "@/renderer/utils/createLogger";
 
-import type { MessageElement } from "../type";
+import { resource } from "./resource";
+import { chatObserverManager } from "../chatObserverManager";
 
-async function setupRevealMask() {
+const log = createLogger("revealMask");
+const awaitInsertMask = new Map<string, { messageEl: HTMLElement; msgRecord: any }>();
+
+function setupRevealMask() {
   if (window.CSS && CSS.registerProperty) {
     CSS.registerProperty({
       name: "--reveal-center",
@@ -33,16 +37,36 @@ async function setupRevealMask() {
     });
   }
 
-  const turbulence = await importBlobAsset("../resources", "turbulence.png");
-  resource.url = URL.createObjectURL(turbulence);
-  console.log(resource);
+  importBlobAsset("../resources", "turbulence.png").then((turbulence) => {
+    resource.url = URL.createObjectURL(turbulence);
+  });
+
+  // 处理异步插入插槽
+  chatObserverManager.addTask({
+    name: "InsertMask",
+    selector: ".pic .image",
+    handler: (elements) => {
+      elements.forEach((mlItem) => {
+        const inserted = awaitInsertMask.get(mlItem.id);
+        if (inserted) {
+          const targetEl = inserted.messageEl.querySelector<HTMLElement>(".reply-content .pic .image");
+          if (targetEl) {
+            const mask = document.createElement("reveal-mask");
+            mask.setupTarget(targetEl);
+            targetEl.insertAdjacentElement("afterbegin", mask);
+            awaitInsertMask.delete(inserted.msgRecord.msgId);
+          }
+        }
+      });
+    },
+  });
 }
 
 function revealMask(component: any, isNewVersion: boolean) {
   if (isNewVersion) {
     // 处理新版本逻辑
   } else {
-    const messageEl = component.vnode.el as MessageElement;
+    const messageEl = component.vnode.el as HTMLElement;
     const msgRecord = component.props.msgRecord;
     const buddyMapInfo = component.props.buddyMapInfo;
     const maskConfig = configStore.value.message.revealMask;
@@ -64,16 +88,13 @@ function revealMask(component: any, isNewVersion: boolean) {
         }
       }
 
-      if (element.replyElement && element.replyElement.sourceMsgIsIncPic) {
+      if (element.replyElement) {
         const replyUin = buddyMapInfo[element.replyElement.senderUidStr];
         if (maskConfig.allUsers || list.includes(replyUin)) {
-          const mask = document.createElement("reveal-mask");
-          const targetEl = messageEl.querySelector<HTMLElement>(".reply-content");
-          console.log("replyElement2", messageEl.outerHTML);
-          if (targetEl) {
-            mask.setupTarget(targetEl);
-            targetEl.insertAdjacentElement("afterbegin", mask);
-          }
+          const messageEl = component.vnode.el as HTMLElement;
+          const msgRecord = component.props.msgRecord;
+          log("注册异步插入遮罩层");
+          awaitInsertMask.set(msgRecord.msgId, { messageEl, msgRecord });
         }
       }
     });
