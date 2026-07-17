d32b3cb5d3dfef98ba4069eca7cbe2e76e950207
xiyuesaves <xiyuesaves@gmail.com>
2026-07-17 10:42:16 +0800

    chore: 调整导入路径

--- src/renderer/pages/devWindow.ts
+++ src/renderer/pages/devWindow.ts
@@ -2,7 +2,7 @@ import { setupLocalStickers } from "@/renderer/modules/localStickers";
 import { styleManager } from "@/renderer/modules/styleManager";
 import { StickerContainer } from "@/renderer/modules/localStickers";
 import { configStore } from "@/renderer/modules/configStore";
-import { setupRevealMask } from "@/renderer/modules/revealMask";
+import { setupRevealMask } from "@/renderer/modules/handleMessages/revealMask";
 
 document.addEventListener("DOMContentLoaded", () => {
   setTimeout(async () => {
