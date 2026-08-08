993e2941372391ab1461472fcbbcccea7c846651
xiyuesaves <xiyuesaves@gmail.com>
2026-08-08 11:13:49 +0800

    feat: 完成入口

--- src/main/modules/messages/sendMessageInterceptor/index.ts
+++ src/main/modules/messages/sendMessageInterceptor/index.ts
@@ -2,28 +2,33 @@ import { configManager } from "@/main/modules/configManager";
 import { checkChatType } from "@/common/checkChatType";
 import { createLogger } from "@/main/utils/createLogger";
 
+import { convertVideoFileToVideoMsg } from "./convertVideoFileToVideoMsg";
+import { largPicFileToPicMsg } from "./largPicFileToPicMsg";
+
 const log = createLogger("outgoingMessageInterceptor");
 
 function handleSendMessages(...args: any[]) {
   try {
-    log(args[3]);
-    const msgPayload = args?.[3]?.[1];
+    const msgPayload = args?.[3]?.[1]?.[0];
     if (!msgPayload) return;
-    const originMsgPayload = structuredClone(msgPayload);
-    if (checkChatType(originMsgPayload.payload[0].peer)) {
-      processMessages(originMsgPayload, msgPayload);
+    log(msgPayload);
+    // const originMsgPayload = structuredClone(msgPayload);
+    if (checkChatType(msgPayload.peer)) {
+      processMessages(msgPayload);
     }
   } catch (err: any) {
     log("出现错误", err.message, err?.stack);
   }
 }
 
-function processMessages(originMsgPayload: any, msgPayload: any) {
+function processMessages(msgPayload: any) {
   try {
     const config = configManager.value;
     if (config.message.videoFileToVideoMsg) {
+      convertVideoFileToVideoMsg(msgPayload);
     }
     if (config.message.largPicFileToPicMsg) {
+      largPicFileToPicMsg(msgPayload);
     }
   } catch (err: any) {
     log("出现错误", err.message, err?.stack);
